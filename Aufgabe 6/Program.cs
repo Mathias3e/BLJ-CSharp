02377575c8bce1dcbb74a26fd6eff10a9c742536
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-29 08:49:20 +0200

    Aufgabe 6 fertig

---
+++ Aufgabe 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------\nKleines 1x1\n-----------\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int i2 = 1; i2 <= 10; i2++)
+                {
+                    Console.Write($"{i2 * i}\t");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
