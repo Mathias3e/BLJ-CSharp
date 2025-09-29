4c7debbb6d3f1fd293f95072028f108288858606
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-29 15:57:28 +0200

    Aufagbe 14

---
+++ Aufgabe 14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int jahr;
+
+            Console.WriteLine("Prüfen, ob es sich bei einem Jahr um ein Schaltjahr handelt.\n************************************************************");
+            do
+            {
+                do
+                {
+                    Console.Write("\nEingabe Jahr (q to quit): ");
+                    input = Console.ReadLine();
+                } while (int.TryParse(input, out jahr) == false && input != "q");
+
+                Console.Write("\n");
+
+                if ((jahr % 4 == 0 && jahr % 100 != 0) || (jahr % 400 == 0 && jahr % 4000 != 0))
+                {
+                    Console.Write($"Das Jahr {jahr} ist ein Schaltjahr.");
+                }
+                else
+                {
+                    Console.Write($"Das Jahr {jahr} ist KEIN Schaltjahr.");
+                }
+
+                Console.Write("\n");
+            } while (input != "q");
+        }
+    }
+}
