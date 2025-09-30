658adc57ab0fe3ca97852f7c5524be1e8ace98ec
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-30 09:14:33 +0200

    Aufgabe 16 fertig

--- Aufgabe 16/Program.cs
+++ Aufgabe 16/Program.cs
@@ -1,14 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace TryParse
 {
-    static void Main()
+    internal class Program
     {
-        int color = 0;
-        for (int i = 0; i <= 255; i++)
+        static void Main(string[] args)
         {
-            Console.Write("\x1b[38;2;37;150;190m*");
+            Random random = new Random();
+
+            string input;
+            int secretNumber, guess = 0, attempts;
+
+            do
+            {
+                attempts = 0;
+                secretNumber = random.Next(1, 101);
+                do
+                {
+                    attempts++;
+
+                    do
+                    {
+                        if (attempts == 1)
+                        {
+                            Console.WriteLine("Deine Zahl (1..100): ");
+                        }
+                        else if (guess < secretNumber && guess > 0)
+                        {
+                            Console.WriteLine("Zahl ist zu klein! Nächster Versuch: ");
+                        }
+                        else if (guess > secretNumber && guess < 101)
+                        {
+                            Console.WriteLine("Zahl ist zu gross! Nächster Versuch: ");
+                        }
+                        else if (guess > 100 || guess < 1)
+                        {
+                            Console.WriteLine("Ist kien oder Zahl ist nicht zwischen 1 und 100. Nächster Versuch: ");
+                        }
+
+                        input = Console.ReadLine();
+                    } while (int.TryParse(input, out guess) == false);
+
+                } while (guess != secretNumber);
+
+                Console.WriteLine($"Die Zahl stimmt! Du hast total {attempts} Versuche benötigt. Noch einmal spielen? [y/n]");
+                input = Console.ReadLine();
+
+            } while (input.ToLower() == "y");
+
+            Console.ReadKey();
         }
-        Console.ReadKey();
     }
 }
