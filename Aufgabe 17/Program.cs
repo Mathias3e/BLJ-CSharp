98b6dec428c95fb4bf050b57e181c047e2055944
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-30 10:03:57 +0200

    Aufagbe 17 fertug

---
+++ Aufgabe 17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            DateTime today = DateTime.Today;
+            DateTime birthday;
+            TimeSpan age;
+
+            Console.Write("Bitte gib ein Geburtsdatum ein: ");
+
+            do
+            {
+                Console.Write("Bitte gib ein gültiges Datum ein: ");
+                input = Console.ReadLine();
+            } while (DateTime.TryParse(input, out birthday) == false);
+
+            age = today - birthday;
+
+            Console.WriteLine($"\nJahren: {age.Days/356}");
+            Console.WriteLine($"Monaten: {age.Days/30}");
+            Console.WriteLine($"Wochen: {age.Days/7}");
+            Console.WriteLine($"Tagen: {age.Days}");
+
+            Console.ReadKey();
+        }
+    }
+}
