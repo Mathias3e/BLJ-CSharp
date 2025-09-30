fa7007747fc25cbe2bae95a55426afc4c42d49e9
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-30 11:12:57 +0200

    Aufgabe 19 ferig

---
+++ Aufgabe 19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int[] calculation = new int[2];
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Make your calculation (or press Q to quit):");
+                    input = Console.ReadLine();
+
+                    if (input.ToLower() == "q") { return; }
+
+                    try
+                    {
+                        calculation = input.Split('+', '-', '/', '*').Select(int.Parse).ToArray();
+                        break;
+                    }
+                    catch { }
+                } while (true);
+
+
+                if (input.Contains('+'))
+                {
+                    Console.WriteLine(calculation[0] + calculation[1]);
+                }
+                else if (input.Contains('-'))
+                {
+                    Console.WriteLine(calculation[0] - calculation[1]);
+                }
+                else if (input.Contains('/'))
+                {
+                    Console.WriteLine(calculation[0] / calculation[1]);
+                }
+                else if (input.Contains('*'))
+                {
+                    Console.WriteLine(calculation[0] * calculation[1]);
+                }
+            } while (true);
+        }
+    }
+}
