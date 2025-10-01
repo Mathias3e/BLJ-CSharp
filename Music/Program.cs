3fe0f66ba21821db62d42b09ddf821bfc5ee2cf5
Mathias3e <mathias.staedelin@ckw.ch>
2025-10-01 13:36:57 +0200

    Add music to C#

---
+++ Music/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayMusic("Audio.wav");
+            Console.ReadKey();
+        }
+
+        public static void PlayMusic(string filepath)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filepath;
+            musicPlayer.Play();
+        }
+    }
+}
