733b27b168fdb5632eb30c581c64d6f63fdd95f6
Mathias3e <mathias.staedelin@ckw.ch>
2025-10-03 11:16:16 +0200

    test mit list

---
+++ Test Arery_Json/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_Array_Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> contact = new List<string[]>();
+
+            contact.Add(new string[] { "1", "a", "#" });
+            contact.Add(new string[] { "2", "b", "@" });
+
+            Console.Write(contact[1][1]);
+
+            Console.ReadKey();
+        }
+    }
+}
